b516a3712b715f4127db260481ad984e7ffa619b
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-15 21:45:50 -0400

    got getAllRecipes model and router completed and working

--- api/router.js
+++ api/router.js
@@ -5,5 +5,16 @@ const Recipes = require("./model");
 //INSTANCE OF ROUTER
 const router = express.Router();
 
+//ENDPOINTS
+router.get("/", (req, res, next)=>{
+    Recipes.getAllRecipes()
+    .then((allRecipes)=>{
+        res.status(200).json(allRecipes);
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
 //EXPORTS
 module.exports = router;

80d9b955ff30f320b13c5c09fbf0d427fd1fc285
ahmedseragcodes <ahmedserag1@gmail.com>
2021-04-15 21:58:34 -0400

    wrote all get requests for recipes, recipes instructions, and recipes ingredients, all working

--- api/model.js
+++ api/model.js
@@ -4,6 +4,20 @@ function getAllRecipes(){
     return db("recipes")
 }
 
+function getRecipesIngredients(recipeID){
+    return db("recipes")
+            .join("ingredients", "recipes.recipe_id", "ingredients.recipe_id")
+            .where("recipes.recipe_id", recipeID) 
+}
+
+function getRecipesInstructions(recipeID){
+    return db("recipes")
+            .join("instructions", "recipes.recipe_id", "instructions.recipe_id")
+            .where("recipes.recipe_id", recipeID)
+}
+
 module.exports={
     getAllRecipes,
+    getRecipesIngredients,
+    getRecipesInstructions
 }
--- api/router.js
+++ api/router.js
@@ -6,6 +6,8 @@ const Recipes = require("./model");
 const router = express.Router();
 
 //ENDPOINTS
+
+//[GET] All Recipes 
 router.get("/", (req, res, next)=>{
     Recipes.getAllRecipes()
     .then((allRecipes)=>{
@@ -15,6 +17,31 @@ router.get("/", (req, res, next)=>{
         next(err);
     })
 })
+
+//[GET] All Recipes Ingredients
+
+router.get("/:id/ingredients", (req, res, next)=>{
+    Recipes.getRecipesIngredients(req.params.id)
+    .then((allIngredients)=>{
+        res.status(200).json(allIngredients);
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
+//[GET] All Recipes Instructions
+
+router.get("/:id/instructions", (req,res,next)=>{
+    Recipes.getRecipesInstructions(req.params.id)
+    .then((allInstructions)=>{
+        res.status(200).json(allInstructions);
+    })
+    .catch((err)=>{
+        next(err);
+    })
+})
+
 
 //EXPORTS
 module.exports = router;

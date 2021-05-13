f3b32177da24872d6194b821a59bfcf08103d273
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-13 18:30:10 -0400

    installed dependencies, setup file structure, setup indexjs serverjs

---
+++ index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const server = require("./server");
+const PORT = process.env.PORT || 1234;
+const path = require("path");
+
+server.listen(PORT, ()=>{
+    console.log(`Listening On Port ${PORT}`);
+});
+
+//For Front-End connection if client folder is made and create react app is run within
+server.use(express.static(path.join(__dirname, "client/build")));
+
+server.use("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+    });
+
+//remember to run npm run build before deployment
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const recipesRouter = require("./api/recipes-router");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const server = express();
+
+server.use(express.json());
+server.use("/api/routers", recipesRouter);
+server.use(cors());
+server.use(helmet());
+
+//Global Error Handling 
+server.use((err, req, res, next)=>{
+    res.status(500).json({message: err.message});
+});
+
+//Sanity Check Endpoint
+server.get("*", (req, res, next)=>{
+    res.json({message: "API Up"});
+});
+
+

96e71b34ec90380f4e8c9801bc48c374fd51eb2f
ahmedseragcodes <ahmedserag1@gmail.com>
2021-05-13 20:57:54 -0400

    migration and seeds deployed, organized properly

--- data/seeds/05-rec_ing_data.js
+++ data/seeds/05-rec_ing_data.js
@@ -1,9 +1,9 @@
 
 exports.seed = function(knex) {
 
-  return knex('ingredients').insert([
+  return knex('rec_ing_assign').insert([
     {ingredient_name: "beef burger patty", recipe_id_one: 1},
-    {ingredient_name: "cheese", recipe_id_one: 1, recipe_id_2: 2},
+    {ingredient_name: "cheese", recipe_id_one: 1, recipe_id_two: 2},
     {ingredient_name: "eggs", recipe_id_one: 1},
     {ingredient_name: "avocado", recipe_id_one: 3},
   ]);
